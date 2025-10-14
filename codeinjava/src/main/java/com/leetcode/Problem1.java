4c15e7caee4eb343dee9d7f44a5dd49fbe5feb23
lucianosantos23 <lucianoaraujodossantosfilho@gmail.com>
2025-10-14 15:30:33 -0300

    refactor: problem1 and add: problem 189

--- codeinjava/src/main/java/com/leetcode/Problem1.java
+++ codeinjava/src/main/java/com/leetcode/Problem1.java
@@ -1,16 +1,35 @@
 package com.leetcode;
 
-public class Problem1 {
-    public int[] twoSum(int[] nums, int target)
+import java.util.HashMap;
+
+public class Solution {  
+    public class Item
     {
-        int [] indices = new int[2];
-        for( int i = 0; i < nums.length; i++ )
-            for(int j = i+1; j < nums.length; j++)
-                if( nums[i] + nums[j] == target )
-                {
-                    indices[0] = i;
-                    indices[1] = j;
-                }
-        return indices; 
+         int indice;
+         int valor;
+        public Item(int indice, int valor) {
+            this.indice = indice;
+            this.valor =valor;
+        }
+        
+    }
+
+
+    public int[] twoSum(int[] nums, int target) {
+
+
+        HashMap<Integer, Item> hash = new HashMap<>();
+        int [] resultado = new int [2];
+        for(int i = 0; i < nums.length; i++)
+        {
+            if( hash.get(nums[i]) != null )
+            {
+                resultado[0] = hash.get(nums[i]).indice; 
+                resultado[1] = i;
+            }
+            hash.put(target-nums[i], new Item(i, nums[i]));
+        }
+
+        return resultado;
     }
 }
---
+++ codeinjava/src/main/java/com/leetcode/Problem169.java
@@ -0,0 +1,13 @@
+package com.leetcode;
+
+import java.util.Arrays;
+
+class Solution {
+    public int majorityElement(int[] nums) {
+
+        Arrays.sort(nums);
+
+        if( nums.length%2 == 0 ) return nums[(nums.length/2)-1]; 
+        else return nums[nums.length/2];
+    }
+} 
